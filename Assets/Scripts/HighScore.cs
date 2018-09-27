eeb031e1db065da91476cbf05d56605733cf133b
Victor <36495431+victorkim17@users.noreply.github.com>
2018-09-26 20:59:12 -0400

    Submission
    Submission for apple picker

---
+++ Assets/Scripts/HighScore.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HighScore : MonoBehaviour {
+    static public int score = 1000;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Text gt = this.GetComponent<Text>();
+        gt.text = "High Score: " + score;
+        if(score > PlayerPrefs.GetInt("HighScore"))
+        {
+            PlayerPrefs.SetInt("HighScore", score);
+        }
+	}
+
+    void Awake()
+    {
+        if (PlayerPrefs.HasKey("HighScore"))
+        {
+            score = PlayerPrefs.GetInt("HighScore");
+        }
+        PlayerPrefs.SetInt("HighScore", score);
+    }
+}
